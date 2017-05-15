137ff18eccc9b1df1b9de968e69e883ccf865e54
taoxf <taoxf@ipharmacare.net>
2017-05-15 21:35:06 +0800

    优化打包速度  include  exclude

---
+++ src/components/layer/layer.js
@@ -0,0 +1,10 @@
+// import tpl from './layer.html';
+
+const layer = function () {
+	return {
+		name:'layer',
+		tpl:tpl
+	}
+}
+
+export default layer;
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,34 @@
-// module.exports = {
-// 	entry: ["./src/script/main.js","./src/script/a.js"] //入口文件
-// 	output: {
-// 		// path:__dirname+'/dist/js',
-// 		// filename:'bundle.js'
-// 		filename:'[name].bundle.js',
-// 		chunkFilename: "[id].bundle.js"
-// 	}
-// }
 var path=require('path');
 var htmlWebpackPlugin = require('html-webpack-plugin');
 module.exports={
 	//entry webpack打包的入口
-    entry:{
-        // bundle:['./src/script/main.js','./src/script/a.js'] //平行的互相依赖的文件打包到一起
-        main:'./src/script/main.js',
-        a:'./src/script/a.js',
-        b:'./src/script/b.js',
-        c:'./src/script/c.js'
-    },
+    entry:'./src/app.js',
     output:{
-    	filename:'js/[name]-[hash].js',
-        path:path.join(__dirname,'./dist'),//两杠的dirname
-        publicPath: 'http://cdn.com'//上线发布地址
+    	filename:'js/[name].bundle.js',
+        path:path.join(__dirname,'./dist')//两杠的dirname
+    },
+    module:{
+        loaders:[
+            {
+                test:/\.js$/,
+                loader:'babel-loader',
+                exclude:[
+                 path.resolve(__dirname, './node_modules/'),
+                ],  //打包排除的范围
+                include:[
+                 path.resolve(__dirname, './src/'),
+                ],
+                query:{
+                    presets:['env']
+                }
+            }
+        ]
     },
     plugins:[
     	new htmlWebpackPlugin({
-    		filename:'a.html',//声明文件的名称
+    		filename:'index.html',//声明文件的名称
     		template:'index.html',
-    		inject:false,//指定脚本所放的位置
-    		title:'webpack is a.html',
-            excludeChunks:['b','c']
-    	}),
-        new htmlWebpackPlugin({
-            filename:'b.html',//声明文件的名称
-            template:'index.html',
-            inject:false,//指定脚本所放的位置
-            title:'webpack is b.html',
-            excludeChunks:['a','c']
-        }),
-        new htmlWebpackPlugin({
-            filename:'c.html',//声明文件的名称
-            template:'index.html',
-            inject:false,//指定脚本所放的位置
-            title:'webpack is c.html',
-            excludeChunks:['a','b']
-        })
+    		inject:'body'//指定脚本所放的位置
+    	})
     ]
 }
