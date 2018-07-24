3147109a83e3c48b7ca4d5e4752a91772aadcbe4
taoxiaofeng55 <taoxiaofeng55@qq.com>
2018-07-24 15:20:57 +0800

    “管理资源

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,38 @@
-var path=require('path');
-var htmlWebpackPlugin = require('html-webpack-plugin');
-module.exports={
-	//entry webpack打包的入口
-    entry:'./src/app.js',
-    output:{
-    	filename:'js/[name].bundle.js',
-        path:path.join(__dirname,'./dist')//两杠的dirname
+var path = require('path');
+// var htmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    //entry webpack打包的入口
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist') //两杠的dirname
     },
-    module:{
-        loaders:[
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
             {
-                test:/\.js$/,
-                loader:'babel-loader',
-                include:path.resolve(__dirname, 'src'),//打包包含的范围,
-                exclude:path.resolve(__dirname, 'node_modules'),//打包排除的范围,  
-                query:{
-                    presets:['env']
-                }
+                test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(csv|tsv)$/,
+                use: [
+                    'csv-loader'
+                ]
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader'
+                ]
             }
         ]
-    },
-    plugins:[
-    	new htmlWebpackPlugin({
-    		filename:'index.html',//声明文件的名称
-    		template:'index.html',
-    		inject:'body'//指定脚本所放的位置
-    	})
-    ]
+    }
 }
