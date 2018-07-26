73bf38d9b8eb1c2f28d05c17548e38f9b21602d7
taoxf <taoxf@ipharmacare.net>
2018-07-26 14:08:18 +0800

    配置中间服务

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function component() {
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    btn.innerHTML = 'Click me and check the console!';
+    btn.innerHTML = '点击我！';
     btn.onclick = printMe;
 
     element.appendChild(btn);
--- src/print.js
+++ src/print.js
@@ -1,7 +1,3 @@
 export default function printMe() {
-    console.log('I get called from print.js!');
-    console.log('I get called from print.js!');
-    console.log('I get called from print.js!');
-    console.log('I get called from print.js!');
-    console.log('I get called from print.js!');
+    console.log('获取 print.js!');
 }
