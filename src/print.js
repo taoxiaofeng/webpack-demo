38f4f9d67faae45312d8f3f6d5d9dbd4089bc17c
taoxf <taoxf@ipharmacare.net>
2018-07-26 11:35:55 +0800

    开发

--- src/print.js
+++ src/print.js
@@ -1,3 +1,7 @@
 export default function printMe() {
     console.log('I get called from print.js!');
+    console.log('I get called from print.js!');
+    console.log('I get called from print.js!');
+    console.log('I get called from print.js!');
+    console.log('I get called from print.js!');
 }
--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,14 @@ module.exports = {
         app: './src/index.js',
         print: './src/print.js'
     },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
     plugins: [
         new CleanWebpackPlugin(['dist']), //清理 /dist 文件夹
         new HtmlWebpackPlugin({ //简化HTML文件的创建，以便为您的webpack包提供服务
-            title: 'Output Management'
+            title: 'Development'
         })
     ],
     output: {
